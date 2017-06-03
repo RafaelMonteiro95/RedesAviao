46ecff6df0f677b0d9f5124ab063138c80c85a01
rafaelmonteiro95 <rafaelmonteiro95@gmail.com>
2017-06-02 23:01:21 -0300

    fixed controller build name, created run.sh to open multiple xterms running clients and one controller

---
+++ run.sh
@@ -0,0 +1,12 @@
+cd sensors/
+make
+cd ../controller/
+make
+cd ../sensors/
+xterm -e make run &
+xterm -e make run &
+xterm -e make run &
+xterm -e make run &
+xterm -e make run &
+cd ../controller/
+xterm -e make run &
